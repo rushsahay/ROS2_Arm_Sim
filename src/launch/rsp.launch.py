54b508a2d3da77c1d297b12b759f02133d1b9ec3
Rushsahay <rushhaank.sahay@gmail.com>
2026-05-19 19:05:36 -0500

    ROS control added to simulate motors at joints

--- src/launch/rsp.launch.py
+++ src/launch/rsp.launch.py
@@ -2,7 +2,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
+from launch import LaunchDescription 
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
@@ -20,6 +22,8 @@ def generate_launch_description():
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file).toxml()
     
+    #Path to controller configuration
+    controller_config = os.path.join(pkg_path,"config","my_controllers.yaml")
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
@@ -29,10 +33,58 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    node_joint_state_publisher = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        output='screen'
+    node_controller_manager_launcher = LaunchDescription([
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            parameters=[
+                {"robot_description":robot_description_config,"update_rate":30,"use_sim_time":use_sim_time},
+                "/home/rushhaank/arm_sim/Arm_Sim/src/config/my_controllers.yaml"
+            ],
+            output="screen"
+        )
+    ])
+
+    rviz = LaunchDescription([
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            parameters=[{"use_sim_time":use_sim_time}],
+            output="screen"
+        )
+    ])
+
+    #Launch Gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory("gazebo_ros"),"launch","gazebo.launch.py"
+        )]),
+        launch_arguments={"verbose":"true"}.items()
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity","3dof_arm","-topic","robot_description"],
+        output="screen",
+        parameters=[{"use_sim_time":use_sim_time}])
+    
+    #Spawn the controllers
+    spawn_joint_state_publisher = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        parameters=[{"use_sim_time":use_sim_time}],
+        output="screen"
+    )
+
+    spawn_arm_position_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        #Pass in config yaml so the controller manager knows gains
+        arguments=["arm_position_controller","--param-file",controller_config],
+        parameters=[{"use_sim_time":use_sim_time}],
+        output="screen"
     )
 
 
@@ -42,6 +94,11 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-
-        node_robot_state_publisher,node_joint_state_publisher
+            gazebo,
+            node_robot_state_publisher,
+            spawn_entity,
+            node_controller_manager_launcher,
+            spawn_joint_state_publisher,
+            spawn_arm_position_controller,
+            rviz
     ])
